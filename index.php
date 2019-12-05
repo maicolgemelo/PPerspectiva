1dc08d9e2e6ec30cb39e52e7a6a56b7d33ae8f4f
Michael Escobar Galeano <maicolgemelo@gmail.com>
2019-12-05 02:21:18 +0100

    programa el cual habla hacerce de la perspectiva en el cual se implementa la @media query por resoluciones

---
+++ index.php
@@ -0,0 +1,151 @@
+<!DOCTYPE HTML>
+<html lang="es">
+        <head>
+                <meta charset="utf-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <title>
+                    explicacion perspectiva
+                </title>
+                <link rel="stylesheet" type="text/css" href="css/estilos.css">
+            </head>
+            <body>
+              
+              <div id="pagewrap">
+
+                    <div id="logo1">
+                            <img class = "imagen1" src="imagenes/Fukuda.jpg" width="200px" height="168">
+                            
+                    </div>
+
+                  <div id="header">
+
+                     <div id="info">
+                            <h1>Perspectiva</h1>
+                            <p>
+                              Todo lo que escuchamos es una opinión,no un hecho.
+                              Todo lo que vemos es una perspectiva, no es la realidad.
+                            </p>
+                      </div>
+                      <div id="logo2">
+                            <img class = "imagen2" src="imagenes/Fukuda.jpg" width="200px" height="168">
+                            
+                      </div>
+                    
+                  </div>
+                  <header>
+                    <nav>
+                      <ul>
+                        <li><a href="#">Inicio</a></li>
+                        <li><a href="#">mision</a></li>
+                        <li><a href="#">vision</a></li>
+                        <li><a href="#">contactenos</a></li>
+                      </ul>
+                    </nav>
+                  </header>
+                  
+
+                  
+
+                
+
+                 <div id="content">
+                   <h2>Perspectiva:</h2>
+                    <p>
+                     El término perspectiva (en latín, perspicere "para ver a través de")1​ se utiliza en las artes gráficas para designar a una representación,
+                     generalmente sobre una superficie plana (como el papel o un lienzo), de un motivo tal como es percibido por la vista, de forma que se pueda
+                     intuir su configuración tridimensional.
+                    </p>
+                    <p>
+                     Geométricamente, estas representaciones se obtienen a partir de la intersección de un plano [el plano del dibujo] con un conjunto de visuales
+                     (las líneas rectas o rayos que unen los puntos del objeto representado con el punto desde el que se observa (denominado el punto de vista)).
+                    </p>
+                    </div>
+                    <div id="conten2">
+                      <div id="parraf3">
+                        <p>
+                            El término perspectiva es usado en múltiples ocasiones, y en la mayoría de esas ocasiones,
+                             se refiere el concepto a cuestiones diversas, por lo cual intentaremos poner un poco de claridad a cuáles usos puede darse a este término 
+                             y cómo es usado en diferentes situaciones y contextos.
+                        </p>
+
+                      </div>
+                      <div id="parraf4">
+                        <p>
+                            la perspectiva es una técnica por la cual se pueden representar objetos en tercera dimensión pero en una superficie que es plana,
+                            como por ejemplo un papel o una tela (lienzos). A partir de esa perspectiva de tercera dimensión, al objeto se le atribuye una sensación de profundidad y de volumen. 
+                            Es utilizada de este modo la perspectiva cuando necesitamos dibujar cubos por ejemplo, y en arquitectura se utiliza mucho cuando se deben realizar representaciones de obras en tercera dimensión 
+                            (en la actualidad, mucho puede contribuir a esto la utilización de programas informáticos que facilitan la tarea de dibujar).
+                        </p>
+
+                      </div>
+                    </div>
+
+                    <div id="sidebars">
+                        <div id="sidebar1">
+                            <h3>Importancia de la Geografía Humana</h3>
+                            <p>Entendemos por geografía humana a una de las ramas de la geografía que tiene por objetivo el estudio y análisis de las relaciones que se establecen
+                              entre el ser humano y el medio ambiente en el que vive y al cual transforma permanentemente.
+                            </p>
+                            <p>
+                              La geografía humana es una de las menos conocidas en términos generales pero una de las más importantes para entender los fenómenos que 
+                              ocurren en el planeta que vivimos y que muchas veces son consecuencia de la acción del hombre.
+                            </p>
+                            <p>La geografía humana es específica y tiene como interés sólo el análisis de aquellos fenómenos o realidades en los que participa el ser humano y que se dan en un entorno geográfico. 
+                              Así, la disciplina sirve para comprender las relaciones que se establecen entre las dos partes y los posibles lazos de transformación que una le puede generar a la otra. 
+                              Si bien en la Antigüedad la fuerza del entorno y del ambiente en la calidad de vida del ser humano era mucho más fuerte e incluso incontrolable, 
+                              hoy en día esa relación de fuerzas se ha invertido y es el ser humano quien en la mayoría de los casos altera la naturaleza que lo rodea, 
+                              generando en ocasiones cambios indelebles e incontrolables como el famoso y peligrosísimo cambio climático.
+                            </p>
+         
+                        </div>
+                        <div id="sidebar2">
+                          <h3>Importancia del Espacio Geográfico</h3>
+                          <p>
+                            El espacio geográfico es un concepto propio de la geografía y que 
+                            se enfoca en tratar al territorio desde la perspectiva de las consideraciones que la sociedad tiene del mismo,
+                            o desde la perspectiva de las actividades que allí se llevan a cabo.
+                          </p>
+                          <p>
+                            En otras palabras, el espacio geográfico alude al territorio socialmente considerado ya sea por cuestiones políticas, económicas o meramente vivenciales. 
+                            El concepto es relevante porque pone en énfasis la relación existente entre las comunidades y los lugares en las que las mismas se desarrollan. 
+                            Continuamente hacemos referencia a lugares no solo desde la perspectiva de la ubicación, 
+                            sino también desde el sentido que la sociedad les otorga.
+                          </p>
+                          <p>
+                            Un aspecto fundamental del espacio geográfico es el de las actividades económicas. En efecto, las sociedades utilizan el medio ambiente para el desarrollo de distintas actividades productivas, 
+                            actividades para las que suelen tener algún tipo de ventaja. Este tipo de actividad va modificando paulatinamente el paisaje de diversas formas. 
+                            De esta manera, es posible hacer referencia a un ámbito rural, a un ámbito urbano, a un ámbito industrial, etc. Si lo pensamos detenidamente, 
+                            existe una relación estrecha entre esta modificación y las ventajas que una zona ofrece con respecto a alguna actividad específica de índole productiva.
+                          </p>
+                        </div>
+
+
+                    </div>
+
+                    <div id="footer" >
+                      <div id="foto1">
+                          <img class = "perspect1" src="imagenes/perspectiva.jpg" width="200px" height="168">
+
+                      </div>
+                      <div id="contactenos">
+                        <p><b>Contactenos</b></p>
+                        <p>Michael Escobar Galeano</p>
+                        <p>Tècnico laboral en asistente en anàlisis y desarrollo de software</p>
+
+                      </div>
+                      <div id="foto2">
+                          <img class = "perspect2" src="imagenes/image.jpg" width="200px" height="168">
+
+                      </div>
+
+
+                    </div>
+
+                 
+                 
+                 
+              
+            </body>
+
+
+</html>
